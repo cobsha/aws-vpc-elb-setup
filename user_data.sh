97dd384260069912a976f84617ec11233b423bae
cobsha <71638921+cobsha@users.noreply.github.com>
2022-08-01 14:07:26 +0530

    userdata

--- user_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-yum install httpd php git -y
-systemctl enable httpd --now
-
-git clone https://github.com/cobsha/aws-elb-site.git /var/website/
-cp -r /var/website/* /var/www/html/
-chown -R apache:apache /var/www/html
